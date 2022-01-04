b483973ff42877fa4bde7b378ac23cacda680628
SoumyaMhaske325 <83540797+SoumyaMhaske325@users.noreply.github.com>
2022-01-04 17:56:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+menu_list_array = ["Veg Margherita Pizza",
+                    "Chicken tandoori pizza",
+                    "Veg supreme pizza",
+                    "Paneer tikka pizza",
+                    ];
+
+function getmenu(){
+var htmldata;
+
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+//Complete the code
+
+}
+
+function add_top(){
+//Complete the code
+}
